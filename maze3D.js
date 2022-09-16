3ce86748a57e017cf690e02039b3a0d39d17c4dc
GoncharikAnton <61192916+GoncharikAnton@users.noreply.github.com>
2022-09-16 20:23:12 +0300

    Added class Maze#d with toString method. (First commit)

---
+++ maze3D.js
@@ -0,0 +1,46 @@
+class Maze3D {
+    #maze
+
+    constructor(maze) {
+        this.#maze = maze;
+    }
+
+    toString() {
+        for (let q = 0; q < this.#maze.length; q++) {
+            console.log(`\nLevel ${q}\n`);
+            for (let i = 0; i < this.#maze[q].length; i++) {
+                let strTop = '';
+                let strMid = '';
+                let strBottom = '';
+                const nextRow = this.#maze[q][i + 1];
+                for (let j = 0; j < this.#maze[q][0].length; j++) {
+                    const cell = this.#maze[q][i][j];
+                    const nextCell = this.#maze[q][i][j + 1];
+                    cell[2] ? strTop += '    ' : strTop += '+--+';
+                    cell[0] ? strMid += ' ' : strMid += '|';
+                    if (cell[4] && cell[5]) {
+                        strMid += '↕';
+                    } else {
+                        cell[4] ? strMid += '↑' : strMid += ' ';
+                        cell[5] ? strMid += '↓' : strMid += ' ';
+                    }
+                    if (nextCell) {
+                        cell[1] === nextCell[0] ? strMid += ' ' : strMid += '|';
+                    } else {
+                        cell[1] ? strMid += ' ' : strMid += '|';
+                    }
+                    if (!nextRow) {
+                        cell[3] ? strBottom += '    ' : strBottom += '+--+';
+                    }
+
+                }
+                console.log(strTop);
+                console.log(strMid);
+                strBottom && console.log(strBottom);
+                strBottom = ''
+                strMid = ''
+                strTop = ''
+            }
+        }
+    }
+}
